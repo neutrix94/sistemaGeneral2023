2422ec45ce0fb0b4031e9d99e12908a8d245fe98
“neutrix94” <oscar_mdp10@hotmail.com>
2024-01-17 20:42:37 -0600

    Creacion de apis de busqueda de impresion remota 2024 2

--- rest/print/index.php
+++ rest/print/index.php
@@ -10,14 +10,13 @@ require '../src/config/db.php';         // DB Connect CL
 //require '../src/config/dbFact.php';     // DB Connect Fact
 $app = new \Slim\App;
 
+//die( 'here' );
 require 'client/sendFile.php';
+require 'client/obtener_archivos_desde_local.php';
+
 require 'server/getPrints.php';
-
-
-
-
-
+require 'server/obtener_archivos_desde_linea.php';
+require 'server/update_print_files_status.php';
 //require 'client/sendFileDirectly.php';
 //require 'server/getPrintsDirectly.php';
-
 $app->run();
---
+++ rest/print/server/update_print_files_status.php
@@ -0,0 +1,32 @@
+<?php
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+/*
+* Endpoint: enviar archivos
+* Path: /send_file
+* Método: POST
+* Descripción: Envia los archivos al servidor destino
+*/
+$app->post('/actualizar_status_archivos', function (Request $request, Response $response){
+	include( '../../conexionMysqli.php' );
+	$file = array();
+	//die( 'one' );
+	$files = $request->getParam( 'ok_rows' );
+	$files_ok = explode(',', $files );
+	foreach ($files_ok as $key => $file_id) {
+		echo $file_id . '<br>';
+	}
+	die('');
+//recibe id de la sucursal
+//	$store_id = $request->getParam( 'destinity_store_id' );
+//obtiene los archivos pendientes de descargar
+	$sql = "";
+	//$stm = $link->query( $sql ) or die( "Error al consultar los archivos por descargar : {$sql} {$link->error}" );
+	//while ( $row = $stm->fetch_assoc() ) {
+	//	$files[] = $row;
+	//}
+//codifica el arreglo de jsons
+	$post_data = json_encode( array( "files"=>$files ) );
+	return $post_data;
+});
+?>
