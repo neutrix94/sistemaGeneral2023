85644b1aeac893a4eeccbdabd7dd6322629f7f6a
“neutrix94” <oscar_mdp10@hotmail.com>
2024-02-27 11:04:38 -0600

    Modificaciones para sincronizar impresoras por sucursal 2

--- code/especiales/controladores/SysImpresorasSucursales.php
+++ code/especiales/controladores/SysImpresorasSucursales.php
@@ -8,36 +8,18 @@
 		function __construct( $connection ){
 			$this->link = $connection;
 		}
-	/*
-		function obtener_ruta_modulo( $id_sucursal, $modulo ){
-		//consulta el nombre de la sucursal
-			$sql = "SELECT 
-						REPLACE( nombre, ' ', '_' ) AS nombre
-					FROM sys_sucursales
-					WHERE id_sucursal = {$id_sucursal}";
-			$stm = $this->link->query( $sql ) or die( "Error al consultar el nombre de la sucursal : {$this->link->error}" );
-			$sucursal = $stm->fetch_assoc();
-		//consulta el nombre de la carpeta del modulo
-			$sql = "SELECT 
-						nombre_carpeta_modulo
-					FROM sys_modulos_impresion
-					WHERE id_modulo_impresion = {$modulo}";
-			$stm = $this->link->query( $sql ) or die( "Error al consultar el nombre de la carpeta del modulo : {$this->link->error}" );
-			$ruta = $stm->fetch_assoc();
-			return "{$sucursal['nombre']}/{$ruta['nombre_carpeta_modulo']}";
-		}*/
 	//creacion de registros de sincronizacion
-		function crearRegistrosSincronizacionImpresorasSucursales( $tipo, $tabla, $id_registro ){
+		function crearRegistrosSincronizacionImpresorasSucursales( $tipo, $tabla, $id_registro, $link = null ){
 			$json = "{
 				    \"table_name\" : \"sys_impresoras_sucursales\",
 				    \"action_type\" : \"{$tipo}\",
 				    \"primary_key\" : \"id_impresora_sucursal\",
-				    \"primary_key_value\" : \"{$id_registro}\"";
+				    \"primary_key_value\" : \"{$id_registro}\"";//die('here');
 			if( $tipo == 'insert' || $tipo == 'update' ){
 			//consulta datos del registro
 				$sql = "SELECT * FROM sys_impresoras_sucursales WHERE id_impresora_sucursal = {$id_registro}";
-				$stm = $this->link->query( $sql ) or die( "Error al consultar datos de la carpeta para sincronizacion : {$this->link->error}" );
-				$row = $stm->fetch_assoc();
+				$stm = mysql_query( $sql ) or die( "Error al consultar datos de la carpeta para sincronizacion : " );//$this->link->query{$this->link->error}
+				$row = mysql_fetch_assoc($stm);
 				$json .= ", 
 					\"id_impresora_sucursal\" : \"{$row['id_impresora_sucursal']}\",
 					\"id_sucursal\" : \"{$row['id_sucursal']}\",
@@ -57,7 +39,7 @@
 						1
 					FROM sys_sucursales
 					WHERE id_sucursal = {$row['id_sucursal']}";//die( $sql );
-			$stm = $this->link->query( $sql ) or die( "Error al insertar registros de sincronizacion de {$tabla} : {$this->link->error}" );
+			$stm = mysql_query( $sql ) or die( "Error al insertar registros de sincronizacion de {$tabla} : {$sql} : " . mysql_error() );
 			return 'ok';
 		}
 	}
