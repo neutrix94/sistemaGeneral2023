1d67ab46a2a4f34746c7bdaf3c3ec6a6a9c523cb
“neutrix94” <oscar_mdp10@hotmail.com>
2024-04-10 18:19:46 -0600

    Generacion/verificacion de Token 5

--- rest/netPay/server/subirPeticionNetPay.php
+++ rest/netPay/server/subirPeticionNetPay.php
@@ -8,6 +8,26 @@
 	* Descripción: Subir peticion de NetPay
 	*/
 	$app->post('/subir_peticion_netPay', function (Request $request, Response $response){
+		$db = new db();
+		$db = $db->conectDB();
+		$rs = new manageResponse();
+		$vt = new tokenValidation();
+		$token =  (empty($request->getHeader('Token'))) ? '' : implode(" ",$request->getHeader('Token'));
+		if (empty($token) || strlen($token)<36 ) {
+		//Define estructura de salida: Token requerido
+			return $rs->errorMessage($request->getParsedBody(),$response, 'Token_Requerido', 'Se requiere el uso de un token', 400);
+		}else{
+		  //Consulta vigencia
+			try{
+				$resultadoToken = $vt->validaToken($token);
+			if ($resultadoToken->rowCount()==0) {
+				return $rs->errorMessage($request->getParsedBody(),$response, 'Token_Invalido', 'El token proporcionado no es válido', 400);
+			}
+			}catch (PDOException $e) {
+				return $rs->errorMessage($request->getParsedBody(),$response, 'CL_Error', $e->getMessage(), 500);
+			}
+		}
+		
 		if( !include( '../../conexionMysqli.php' ) ){
 			die( "No se pudo incluir el archivo de conexion!" );
 		}
