8f9dcf8a744d7efba5a0c6b676231952ec6baaf3
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-07-05 16:05:21 -0600

    Modificaciones de pantalla de cobros 3.1

--- code/especiales/tesoreria/cobrosSmartAccounts/websocket_client/websocket_client.js
+++ code/especiales/tesoreria/cobrosSmartAccounts/websocket_client/websocket_client.js
@@ -230,7 +230,7 @@ const connectWebSocket = (ws_ref) => {
 					<button
 						type="button"
 						class="btn btn-success"
-						onclick="marcar_notificacion_vista( '${ws.actualTransaction.traceability.folio_unico_transaccion}', ${transaction.traceability.folio_unico_transaccion.trim() == 'Transaccion Exitosa' ? true : false } );"
+						onclick="marcar_notificacion_vista( '${ws.actualTransaction.traceability.folio_unico_transaccion}', ${ws.actualTransaction.message.trim() == 'Transaccion Exitosa' ? true : false } );"
 					><i class="icon-ok=circle">Aceptar y marcar notificacion como vista</i>
 					</button>
           </div>` );
@@ -245,7 +245,7 @@ const connectWebSocket = (ws_ref) => {
         <button
           type="button"
           class="btn btn-success"
-          onclick="marcar_notificacion_vista( '${jsonMsg.transaction.folio_unico}', ${transaction.traceability.folio_unico_transaccion.trim() == 'Transaccion Exitosa' ? true : false } );"
+          onclick="marcar_notificacion_vista( '${jsonMsg.transaction.folio_unico}', ${jsonMsg.transaction.message.trim() == 'Transaccion Exitosa' ? true : false } );"
         ><i class="icon-ok=circle">Aceptar y marcar notificacion como vista</i>
         </button>
         </div>` );
