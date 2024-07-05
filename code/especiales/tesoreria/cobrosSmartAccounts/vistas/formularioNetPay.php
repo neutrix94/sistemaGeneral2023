98ddd097c35e545d0167dc85a48970e53f2ae09d
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-07-04 19:36:03 -0600

    Modificaciones en funcionamiento de pantalla de cobros parte 2

--- code/especiales/tesoreria/cobrosSmartAccounts/vistas/formularioNetPay.php
+++ code/especiales/tesoreria/cobrosSmartAccounts/vistas/formularioNetPay.php
@@ -23,6 +23,10 @@
 				<i class="icon-cancel-circled">Cancelar y cerrar</i>
 			</button>
 		</div>
+		<div id="contador">00:00:00</div>
+		<button style="display:none;" id="start">Start</button>
+		<button style="display:none;" id="stop">Stop</button>
+		<button style="display:none;" id="reset">Reset</button>
 	</div>
 <?php
 	}else{
@@ -46,10 +50,85 @@
 <script>
 	setTimeout( function (){ 
 		//alert();
+		$( '#start' ).click();
 		informar_folio( '<?php echo $resp->folio_unico_transaccion;?>' );
 	}, 100 );
 // Crea una nueva conexión SSE
 	var server_url = 'ajax/server_events.php?transaction_id=<?php echo $resp->folio_unico_transaccion;?>';
 	var emergent_count_tmp = <?php echo $counter;?>;
-	
+	let horas = 0;
+
+/**/
+	let minutos = 0;
+	let segundos = 0;
+	let segundos_totales = 0;
+	let intervalo;
+
+	function actualizarContador() {
+		segundos++;
+		segundos_totales++;
+		if( segundos_totales == $( '#max_execution_time' ).val() ){
+			var content = `<div class="bg-danger text-center">
+				<br>
+				<br>
+				<h2 class="text-light">Tiempo de espera agotado!</h2>
+				<h2 class="text-light">Recarga la pagina y vuelve a escanear el ticket!</h2>
+				<br>
+				<br>
+				<div class="row">
+					<div class="col-3"></div>
+					<div class="col-6 text-center">
+						<button
+							type="button"
+							class="btn btn-warning form-control"
+							onclick="location.reload();"
+							style="font-size : 200% !important; border : 1px solid black;"
+						>
+							<i class="icon-spin3">OK</i>
+						</button>
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>`;
+			$( '.emergent_content' ).html( content );
+			$( '.emergent' ).css( 'display', 'block' );
+			return false;
+		}
+		if (segundos == 60) {
+			segundos = 0;
+			minutos++;
+		}
+
+		if (minutos == 60) {
+			minutos = 0;
+			horas++;
+		}
+
+		const formatoHoras = horas.toString().padStart(2, '0');
+		const formatoMinutos = minutos.toString().padStart(2, '0');
+		const formatoSegundos = segundos.toString().padStart(2, '0');
+
+		document.getElementById('contador').innerText = `${formatoHoras}:${formatoMinutos}:${formatoSegundos}`;
+	}
+
+	document.getElementById('start').addEventListener('click', () => {
+		if (!intervalo) {
+			intervalo = setInterval(actualizarContador, 1000);
+		}
+	});
+
+	document.getElementById('stop').addEventListener('click', () => {
+		clearInterval(intervalo);
+		intervalo = null;
+	});
+
+	document.getElementById('reset').addEventListener('click', () => {
+		clearInterval(intervalo);
+		intervalo = null;
+		horas = 0;
+		minutos = 0;
+		segundos = 0;
+		document.getElementById('contador').innerText = '00:00:00';
+	});
 </script>
