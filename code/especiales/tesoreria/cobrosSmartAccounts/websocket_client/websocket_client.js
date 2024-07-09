7119fcd633874553c44b847a2ef3d975c8ac1fe3
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-07-09 13:50:47 -0600

    Ajuste en websocket_client.js

--- code/especiales/tesoreria/cobrosSmartAccounts/websocket_client/websocket_client.js
+++ code/especiales/tesoreria/cobrosSmartAccounts/websocket_client/websocket_client.js
@@ -230,7 +230,7 @@ const connectWebSocket = (ws_ref) => {
 					<button
 						type="button"
 						class="btn btn-success"
-						onclick="marcar_notificacion_vista( '${ws.actualTransaction.traceability.folio_unico_transaccion}', ${ws.actualTransaction.message.trim() == 'Transacción exitosa' ? true : false } );"
+						onclick="marcar_notificacion_vista( '${ws.actualTransaction.traceability.folio_unico_transaccion}', ${ws.actualTransaction.message.trim() == 'Transacción exitosa' || jsonMsg.transaction.message.trim() == 'Transaccion exitosa' ? true : false } );"
 					><i class="icon-ok=circle">Aceptar y marcar notificacion como vista</i>
 					</button>
           </div>` );
@@ -245,7 +245,7 @@ const connectWebSocket = (ws_ref) => {
         <button
           type="button"
           class="btn btn-success"
-          onclick="marcar_notificacion_vista( '${jsonMsg.transaction.folio_unico}', ${jsonMsg.transaction.message.trim() == 'Transacción exitosa' ? true : false } );"
+          onclick="marcar_notificacion_vista( '${jsonMsg.transaction.folio_unico}', ${jsonMsg.transaction.message.trim() == 'Transacción exitosa' || jsonMsg.transaction.message.trim() == 'Transaccion exitosa' ? true : false } );"
         ><i class="icon-ok=circle">Aceptar y marcar notificacion como vista</i>
         </button>
         </div>` );
