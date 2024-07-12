b17107c75abcecebd12d27b9969a8f89bf23d38d
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-07-12 11:14:21 -0600

    Modificaciones PP 2.2

--- rest/v1/server/inserta_movimientos_proveedor_producto.php
+++ rest/v1/server/inserta_movimientos_proveedor_producto.php
@@ -70,13 +70,13 @@ $app->post('/inserta_movimientos_proveedor_producto', function (Request $request
 
 
 /*valida que las apis no esten bloqueadas*/
-  $validation = $SynchronizationManagmentLog->validate_apis_are_not_locked( $log['origin_store'] );
+  $validation = $SynchronizationManagmentLog->validate_apis_are_not_locked( $log['origin_store'], ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   if( $validation != 'ok' ){
-    $SynchronizationManagmentLog->updateSynchronizationStatus( $log['origin_store'], 2 );
+    $SynchronizationManagmentLog->updateSynchronizationStatus( $log['origin_store'], 2, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
     return $validation;
   } 
 //actualiza indicador de sincronizacion en tabla
-  $update_synchronization = $SynchronizationManagmentLog->updateSynchronizationStatus( $log['origin_store'], 3 );
+  $update_synchronization = $SynchronizationManagmentLog->updateSynchronizationStatus( $log['origin_store'], 3, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   if( $update_synchronization != 'ok' ){
     return $update_synchronization;
   } 
@@ -85,16 +85,16 @@ $app->post('/inserta_movimientos_proveedor_producto', function (Request $request
 
 
 //inserta request
-  $request_initial_time = $SynchronizationManagmentLog->getCurrentTime();
-  $resp["log"] = $SynchronizationManagmentLog->insertResponse( $log, $request_initial_time );
+  $request_initial_time = $SynchronizationManagmentLog->getCurrentTime( ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
+  $resp["log"] = $SynchronizationManagmentLog->insertResponse( $log, $request_initial_time, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   //$pending_petitions = $request->getParam( "pending_responses" );
-  $request_initial_time = $SynchronizationManagmentLog->getCurrentTime();
+  $request_initial_time = $SynchronizationManagmentLog->getCurrentTime( ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   if( sizeof( $product_provider_movements ) > 0 ){
-    $insert_validations = $productProviderMovementsSynchronization->insertProductProviderMovements( $product_provider_movements );
+    $insert_validations = $productProviderMovementsSynchronization->insertProductProviderMovements( $product_provider_movements, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
 //return json_encode( $insert_validations );
     if( $insert_validations["error"] != '' && $insert_validations["error"] != null  ){
     //inserta error si es el caso
-      $resp["log"] = $SynchronizationManagmentLog->updateResponseLog( $insert_validations["error"], $resp["log"]["unique_folio"] );
+      $resp["log"] = $SynchronizationManagmentLog->updateResponseLog( $insert_validations["error"], $resp["log"]["unique_folio"], ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
       $resp["status"] = "error : {$insert_validations["error"]}";
     }else{
       $resp["ok_rows"] = $insert_validations["ok_rows"];
@@ -102,16 +102,16 @@ $app->post('/inserta_movimientos_proveedor_producto', function (Request $request
       $tmp_ok = $insert_validations->tmp_ok;
       $tmp_no = $insert_validations->tmp_no;
     //inserta respuesta exitosa
-      $resp["log"] = $SynchronizationManagmentLog->updateResponseLog( "{$insert_validations["ok_rows"]} | {$insert_validations["error_rows"]}", $resp["log"]["unique_folio"] );
+      $resp["log"] = $SynchronizationManagmentLog->updateResponseLog( "{$insert_validations["ok_rows"]} | {$insert_validations["error_rows"]}", $resp["log"]["unique_folio"], ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
     }
   }else{
   //inserta excepcion controlada
     $response_string = "No llegaron movimientos proveedor producto, posiblemente tengas que bajar el limite de registros de sincronizacion de movimientos proveedor producto!";
-    $resp["log"] = $SynchronizationManagmentLog->updateResponseLog( $response_string, $resp["log"]["unique_folio"] );
+    $resp["log"] = $SynchronizationManagmentLog->updateResponseLog( $response_string, $resp["log"]["unique_folio"], ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   }
 
 /****************************************** Consulta / Envia ******************************************/
-  $config = $SynchronizationManagmentLog->getSystemConfiguration( 'ec_movimiento_detalle_proveedor_producto' );
+  $config = $SynchronizationManagmentLog->getSystemConfiguration( 'ec_movimiento_detalle_proveedor_producto', ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   $path = trim ( $config['value'] );
   $system_store = $config['system_store'];
   $store_prefix = $config['store_prefix'];
@@ -122,20 +122,20 @@ $app->post('/inserta_movimientos_proveedor_producto', function (Request $request
     return json_encode( array( "response"=>"La sucursal es local y no puede ser servidor." ) );
   }
 //ejecuta el procedure para generar los movimientos de almacen
-  $setProductProviderMovements = $productProviderMovementsSynchronization->setNewSynchronizationProductProviderMovements( $log['origin_store'], $system_store, $store_prefix, $rows_limit );
+  $setProductProviderMovements = $productProviderMovementsSynchronization->setNewSynchronizationProductProviderMovements( $log['origin_store'], $system_store, $store_prefix, $rows_limit, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   if( $setProductProviderMovements != 'ok' ){
     return json_encode( array( "response" => $setProductProviderMovements ) );
   }
 //consulta registros pendientes de sincronizar
-  $resp["rows_download"] = $productProviderMovementsSynchronization->getSynchronizationProductProviderMovements( $log['origin_store'], $rows_limit );
+  $resp["rows_download"] = $productProviderMovementsSynchronization->getSynchronizationProductProviderMovements( $log['origin_store'], $rows_limit, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   if ( sizeof( $resp["rows_download"] ) > 0 ) {//inserta request
-    $resp["log_download"] = $SynchronizationManagmentLog->insertPetitionLog( $log['origin_store'], -1, $store_prefix, $initial_time, 'MOVIMIENTOS DE ALMACEN DESDE LINEA' );
+    $resp["log_download"] = $SynchronizationManagmentLog->insertPetitionLog( $log['origin_store'], -1, $store_prefix, $initial_time, 'MOVIMIENTOS DE ALMACEN DESDE LINEA', ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   }
 
-  $SynchronizationManagmentLog->updateModuleResume( 'ec_movimiento_detalle_proveedor_producto', 'subida', $resp["status"], $log["origin_store"] );//actualiza el resumen de modulo/sucursal ( subida )
+  $SynchronizationManagmentLog->updateModuleResume( 'ec_movimiento_detalle_proveedor_producto', 'subida', $resp["status"], $log["origin_store"], ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );//actualiza el resumen de modulo/sucursal ( subida )
   
 //desbloquea indicador de sincronizacion en tabla
-$update_synchronization = $SynchronizationManagmentLog->updateSynchronizationStatus( $log['origin_store'], 2 );
+$update_synchronization = $SynchronizationManagmentLog->updateSynchronizationStatus( $log['origin_store'], 2, ( $LOGGER['id_sincronizacion'] ? $LOGGER['id_sincronizacion'] : false )  );
   return json_encode( $resp );
 
 });
