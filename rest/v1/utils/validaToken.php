6cc7299df16fc900a029fbce55936b7a53940860
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-08-06 21:51:44 -0600

    Se modifica validacion de token

--- rest/v1/utils/validaToken.php
+++ rest/v1/utils/validaToken.php
@@ -1,13 +1,49 @@
 <?php
   class tokenValidation{
+  class tokenValidation{
       public function validaToken($token){
-
+      public function validaToken($token){
+        $db = new db();
         $db = new db();
         $db = $db->conectDB();
+        $db = $db->conectDB();
+        $sqlToken = "SELECT token FROM api_token WHERE token='{$token}' AND expired_in>now();";
         $sqlToken = "SELECT token FROM api_token WHERE token='{$token}' AND expired_in>now();";
         $resultadoToken = $db->query($sqlToken);
-
+        $resultadoToken = $db->query($sqlToken);
+        return $resultadoToken;
         return $resultadoToken;
       }
+      }
+      public function verificaExistenciaToken( $token ){
+        $db = new db();
+        $db = $db->conectDB();
+        $sqlToken = "SELECT token FROM api_token WHERE token='{$token}'";
+        $resultadoToken = $db->query($sqlToken);
+        if( $resultadoToken->rowCount() == 0 ){
+          return $resultadoToken;
+        }else{
+          $valida_caducidad = $this->validaToken($token);
+          if( $valida_caducidad->rowCount() == 0 ){
+            return $this->refrescar_token( $token, $db );
+          }else{
+            return $valida_caducidad;
+          }
+        }
+      }
+      public function refrescar_token( $token, $db ){
+        $sqlAPIConfig="SELECT value FROM api_config c WHERE c.key='token' and name='time_value' limit 1";//Genera sentencia recuperar tiem_value
+        $resultadoConfig = $db->query($sqlAPIConfig);
+        $time_value = $resultadoConfig->fetch();
+        $sqlToken = "UPDATE api_token SET expired_in = ( select TIMESTAMPADD( SECOND, {$time_value['value']}, NOW() ) ) WHERE token = '{$token}'";
+        $stm = $db->query($sqlToken);
+        if( !$stm ){
+          return "Error al renovar token : {$db->errorInfo()}";
+        }else{
+          return $this->validaToken($token);
+        }
+      }
   }
+  }
+?>
 ?>
