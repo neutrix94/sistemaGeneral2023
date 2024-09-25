a76dbfaa5e195d58dced231843824580982f45f2
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-09-25 11:43:22 -0600

    ajuste de etiquetas de precios

--- code/especiales/EtiquetasTermicas/etiquetas_precios_individuales/ajax/TagsGenerator.php
+++ code/especiales/EtiquetasTermicas/etiquetas_precios_individuales/ajax/TagsGenerator.php
@@ -142,10 +142,10 @@
                     $product['price_1'] = $row['price'];
                     $row = $stm->fetch_assoc();
                     $product['number_since'] = $row['number_since'];
-                    $product['price_2'] = ( $row['price'] * $row['number_since'] );
+                    $product['price_2'] = round( $row['price'] * $row['number_since'] );
                 //calcula descuento
                     $discount = ( $product['number_since'] * $product['price_1'] ) - ( $product['number_since'] * $row['price'] );
-                    $product['discount'] = $discount;
+                    $product['discount'] = round($discount);
                     $price = $this->buildTwoPriceHtml( $product );
                     return json_encode( array( "status"=> 200, "templates"=>$price, "product"=>$product) );
                 }
@@ -322,6 +322,26 @@
                 //return $epl_code;
             }
             function MediumTagTwoPricesEPL( $store_id, $user_id, $product ){
+                $price_1 = "";
+            //posiciones primer precio
+                if( $product['price_1'] <= 9 ){
+                    $price_1 = "A290,80,0,3,3,3,N,\"$\"\n";
+                    $price_1 .= "A340,60,0,4,4,5,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A420,110,0,3,3,3,N,\"pz\"\n";
+                }else if( $product['price_1'] <= 99 ){
+                    $price_1 = "A290,80,0,3,3,3,N,\"$\"\n";
+                    $price_1 .= "A340,60,0,4,4,5,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A470,110,0,3,3,3,N,\"pz\"\n";
+                    
+                }else if( $product['price_1'] <= 999 ){
+                    $price_1 = "A220,80,0,3,3,3,N,\"$\"\n";
+                    $price_1 .= "A290,60,0,4,4,5,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A480,110,0,3,3,3,N,\"pz\"\n";
+                }else if( $product['price_1'] >= 1000 ){
+                    $price_1 = "A190,80,0,3,3,3,N,\"$\"\n";
+                    $price_1 .= "A260,60,0,4,4,5,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A520,110,0,3,3,3,N,\"pz\"\n";
+                }
                 $price_size = ( $product['price_1'] <= 999 ? 3 : 2 );
                 $space_1 = ( $product['price'] <= 99 ? ' ' : '' );
                 $space_2 = ( $product['number_since'] <= 9 ? ' ' : '' );
@@ -338,12 +358,19 @@
                 $epl_code .= "f100\n";
                 $epl_code .= "N\n";
                 $epl_code .= "b40,60,Q,m2,s5,\"{$product['list_order']}\"\n";
-                $epl_code .= "A240,60,0,4,5,{$price_size},N,\"{$space_1}{$product['price_1']}\"\n";
-                $epl_code .= "A190,60,0,3,3,3,N,\"{$space_1}$\"\n";
+                /*$epl_code .= "A240,60,0,4,5,{$price_size},N,\"{$space_1}{$product['price_1']}\"\n";
+                $epl_code .= "A190,60,0,3,3,3,N,\"{$space_1}$\"\n";*/
+                $epl_code .= $price_1;
                 $epl_code .= "A40,180,0,4,3,5,N,\"{$space_2}{$product['number_since']}\"\n";
-                $epl_code .= "A160,215,0,3,3,3,N,\"X\"\n";
-                $epl_code .= "A220,210,0,3,3,3,N,\"$\"\n";
-                $epl_code .= "A290,140,0,5,2,{$price_size},N,\"{$product['price_2']}\"\n";
+                if( $product['number_since'] <= 99 ){
+                    $epl_code .= "A160,215,0,3,3,3,N,\"X\"\n";
+                    $epl_code .= "A220,210,0,3,3,3,N,\"$\"\n";
+                }else if( $product['number_since'] > 99 ){
+                    $epl_code .= "A190,215,0,3,3,3,N,\"X\"\n";
+                    $epl_code .= "A240,210,0,3,3,3,N,\"$\"\n";
+                }
+                //$epl_code .= "A290,140,0,5,2,{$price_size},N,\"{$product['price_2']}\"\n";
+                $epl_code .= "A290,180,0,4,4,5,N,\"{$product['price_2']}\"\n";
                 $epl_code .= "A40,290,0,2,3,2,R,\"  Ahorra: \${$product['discount']}  \"\n";
                 $epl_code .= "A40,330,0,3,2,2,N,\"{$product['name_part_one']}\"\n";
                 $epl_code .= "A40,370,0,3,2,2,N,\"{$product['name_part_two']}\"\n";
@@ -397,7 +424,38 @@
                 die( "ok" );
             }
             function BigTagTwoPricesEPL( $store_id, $user_id, $product ){
-                $space_1 = ( $product['price'] <= 99 ? ' ' : '' );
+            //posiciones primer precio
+                if( $product['price_1'] <= 9 ){
+                    $price_1 = "A450,530,2,4,5,3,N,\"$\"\n";
+                    $price_1 .= "A350,570,2,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A220,500,2,4,5,3,N,\"pz\"\n";
+                    $price_1_1 = "A340,700,0,4,5,3,N,\"$\"\n";
+                    $price_1_1 .= "A430,670,0,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1_1 .= "A570,730,0,4,5,3,N,\"pz\"\n";
+                }else if( $product['price_1'] <= 99 ){
+                    $price_1 = "A500,530,2,4,5,3,N,\"$\"\n";
+                    $price_1 .= "A400,570,2,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A170,500,2,4,5,3,N,\"pz\"\n";
+                    $price_1_1 = "A290,700,0,4,5,3,N,\"$\"\n";
+                    $price_1_1 .= "A390,670,0,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1_1 .= "A620,730,0,4,5,3,N,\"pz\"\n";
+                    
+                }else if( $product['price_1'] <= 999 ){
+                    $price_1 = "A600,530,2,4,5,3,N,\"$\"\n";
+                    $price_1 .= "A500,570,2,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A170,500,2,4,5,3,N,\"pz\"\n";
+                    $price_1_1 = "A190,700,0,4,5,3,N,\"$\"\n";
+                    $price_1_1 .= "A290,670,0,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1_1 .= "A620,730,0,4,5,3,N,\"pz\"\n";
+                }else if( $product['price_1'] >= 1000 ){
+                    $price_1 = "A640,530,2,4,5,3,N,\"$\"\n";
+                    $price_1 .= "A560,570,2,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1 .= "A140,500,2,4,4,3,N,\"pz\"\n";
+                    $price_1_1 = "A160,700,0,4,5,3,N,\"$\"\n";
+                    $price_1_1 .= "A240,670,0,5,3,3,N,\"{$product['price_1']}\"\n";
+                    $price_1_1 .= "A670,730,0,4,4,3,N,\"pz\"\n";
+                }
+                $space_1 = "";//( $product['price'] <= 99 ? ' ' : '' );
                 $space_2 = ( $product['number_since'] <= 9 ? ' ' : '' );
                 $price_size = ( $product['price'] <= 999 ? 4 : 3 );
                 $epl_code = "\nI8,A,001\n";
@@ -413,8 +471,9 @@
                 $epl_code .= "f100\n";
                 $epl_code .= "N\n";
                 $epl_code .= "b680,480,Q,m2,s5,\"{$product['list_order']}\"\n";
-                $epl_code .= "A500,570,2,5,3,2,N,\"{$space_1}{$product['price_1']}\"\n";
-                $epl_code .= "A620,560,2,4,5,3,N,\"{$space_1}$\"\n";
+                $epl_code .= $price_1;
+                /*$epl_code .= "A500,570,2,5,3,2,N,\"{$space_1}{$product['price_1']}\"\n";
+                $epl_code .= "A620,560,2,4,5,3,N,\"{$space_1}$\"\n";*/
                 $epl_code .= "A800,440,2,5,3,4,N,\"{$space_2}{$product['number_since']}\"\n";
                 $epl_code .= "A570,370,2,4,4,3,N,\"X\"\n";
                 $epl_code .= "A490,370,2,4,4,3,N,\"$\"\n";
@@ -424,8 +483,9 @@
                 $epl_code .= "A800,80,2,3,3,3,N,\"{$product['name_part_two']}\"\n";
 
                 $epl_code .= "b10,670,Q,m2,s5,\"{$product['list_order']}\"\n";
-                $epl_code .= "A280,670,0,5,3,2,N,\"{$space_1}{$product['price_1']}\"\n";
-                $epl_code .= "A160,690,0,4,5,3,N,\"{$space_1}$\"\n";
+                $epl_code .= $price_1_1;
+                /*$epl_code .= "A280,670,0,5,3,2,N,\"{$space_1}{$product['price_1']}\"\n";
+                $epl_code .= "A160,690,0,4,5,3,N,\"{$space_1}$\"\n";*/
                 $epl_code .= "A10,800,0,5,3,4,N,\"{$space_2}{$product['number_since']}\"\n";
                 $epl_code .= "A235,890,0,4,4,3,N,\"X\"\n";
                 $epl_code .= "A365,950,2,4,4,3,N,\"$\"\n";
