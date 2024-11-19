e247f0ff00519805ca14ab3e8b5d453a21f4129c
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-11-19 12:38:38 -0600

    Se arregla el trigger que inserta registro de sincronizacion de productos sin inventario

--- respaldos/triggers_sistema/insertaProductosSinInventario.sql
+++ respaldos/triggers_sistema/insertaProductosSinInventario.sql
@@ -1,11 +1,20 @@
 DROP TRIGGER IF EXISTS insertaProductosSinInventario|
 DELIMITER $$
 CREATE TRIGGER insertaProductosSinInventario
-AFTER INSERT ON ec_productos_sin_inventario
+BEFORE INSERT ON ec_productos_sin_inventario
 FOR EACH ROW
 BEGIN
 	DECLARE store_id INTEGER;
-	SELECT id_sucursal INTO store_id FROM sys_sucursales WHERE acceso=1;
+    DECLARE prefix VARCHAR(20);
+    DECLARE row_id INT( 11 );
+	SELECT id_sucursal, prefijo INTO store_id, prefix FROM sys_sucursales WHERE acceso=1;
+	/*obtiene el siguiente id*/
+	SELECT 
+		auto_increment into row_id
+	FROM information_schema.tables
+	WHERE table_name = 'ec_productos_sin_inventario'
+	AND table_schema = database();
+    SET new.folio_unico = CONCAT( prefix, '_PSI_', row_id );/*, row_id*/
 	IF( new.sincronizar = 1 )
 	THEN
 		INSERT INTO sys_sincronizacion_registros ( id_sincronizacion_registro, sucursal_de_cambio,
@@ -17,6 +26,8 @@ BEGIN
 			CONCAT('{',
 				'"table_name" : "ec_productos_sin_inventario",',
 				'"action_type" : "insert",',
+				'"primary_key" : "folio_unico",',
+				'"primary_key_value" : "', new.folio_unico,'",',
 				'"id_producto" : "', new.id_producto, '",',
 				'"id_sucursal" : "', new.id_sucursal, '",',
 				'"id_usuario" : "', new.id_usuario, '",',
