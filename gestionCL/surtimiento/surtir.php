885b4dcb0eb2fe788dfe4030d9f71f33227b2e44
salvadorLopez10 <jslb_cafcb10@hotmail.com>
2024-11-22 00:47:59 -0600

    Ajuste para codificación de json_encode y ajuste para validación de códigos de barras para cajas y paquetes

--- gestionCL/surtimiento/surtir.php
+++ gestionCL/surtimiento/surtir.php
@@ -220,9 +220,10 @@ $indiceSurtir = 0;
                 return;
             }
             //Toma únicamente los primeros digitos para omitir los últimos dígitos, correspondientes al identificador único
-            const codigoBase = codigoProducto.split(" ").slice(0, -1).join("");
+            //const codigoBase = codigoProducto.split(" ").slice(0, -1).join("");
 
-            if(listaSurtir[indiceSurtir].codigos_barras.split(",").includes(codigoBase)){
+            //if(listaSurtir[indiceSurtir].codigos_barras.split(",").includes(codigoProducto.split(" ").join(""))){
+              if( validarCodigoProducto(codigoProducto, listaSurtir[indiceSurtir].codigos_barras) ){
                 document.getElementById('surtidoGroup').style.display = 'block';
                 document.getElementById('cantidadSurtida').value = '';//Number(listaSurtir[indiceSurtir].cantidad_solicitada);
                 document.getElementById('cantidadSurtida').focus();
@@ -238,6 +239,37 @@ $indiceSurtir = 0;
             }
 
 
+        }
+
+        function validarCodigoProducto(codigoProducto, codigos_barras) {
+          // Eliminar espacios del código del producto
+          codigoProducto = codigoProducto.replace( '  ', ' ' )
+          const codigoLimpio = codigoProducto.split(" ").join("");
+
+          // Dividir la lista de códigos de barras en un arreglo
+          const listaCodigos = codigos_barras.split(",");
+
+          // Verificar si es un paquete o caja (contiene 'PQ' o 'CJ')
+          const esPaqueteOCaja = codigoProducto.includes("PQ") || codigoProducto.includes("CJ");
+
+          if (esPaqueteOCaja) {
+
+              if( codigoProducto.includes("PQ") ){
+
+                // Validar solo los dígitos iniciales sin el identificador único (últimos 5 caracteres)
+                const baseCodigo = codigoLimpio.slice(0, -5); // Remover el identificador único
+                return listaCodigos.some(codigo => codigo.startsWith(baseCodigo));
+              }else{
+
+                // Validar solo los dígitos iniciales sin el identificador único (últimos 4 caracteres)
+                const baseCodigo = codigoLimpio.slice(0, -4); // Remover el identificador único
+                return listaCodigos.some(codigo => codigo.startsWith(baseCodigo));
+
+              }
+          } else {
+              // Validar coincidencia exacta
+              return listaCodigos.includes(codigoLimpio);
+          }
         }
 
         function noHayExistencia() {
