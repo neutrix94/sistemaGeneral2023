81c6ade9e203d175952c4b2b9bb909b24d43e615
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-11-22 15:00:09 -0600

    Modificaciones √

--- code/especiales/facturacion/clientes/saveContent.php
+++ code/especiales/facturacion/clientes/saveContent.php
@@ -1,5 +1,15 @@
 <?php
+/*
+	* Version Oscar 2024-11-22 para poner permisos de lectrua y escritura a archvivo donde se guardan datos de cedula fiscal en alta de clientes facturacion
+*/
 	$name = "archivoDatosQR.txt";
+	if( file_exists($name) ){
+		chmod($file, 0777);
+	}else{
+		$file = fopen("{$name}", "w");
+		fclose($file);
+		chmod($file, 0777);
+	}
 	$file = fopen("{$name}", "w");
 
 	fwrite($file, $_POST['data'] . PHP_EOL );
