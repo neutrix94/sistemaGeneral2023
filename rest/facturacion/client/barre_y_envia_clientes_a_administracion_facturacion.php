5c0ac7182c8fa21b815b2cc3d34676c48efcd803
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2024-11-29 14:26:56 -0600

    Modificaciones en servicio que envia todas las notas pendientes a administracion de facturacin

--- rest/facturacion/client/barre_y_envia_clientes_a_administracion_facturacion.php
+++ rest/facturacion/client/barre_y_envia_clientes_a_administracion_facturacion.php
@@ -10,7 +10,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 * Descripción: Insercion de devoluciones
 */
 
-$app->post('/barre_y_envia_clientes_a_administracion_facturacion', function (Request $request, Response $response){
+$app->post('/barre_y_envia_ventas_a_administracion_facturacion', function (Request $request, Response $response){
 //libreria de conexion
 	if ( ! include( '../../conexionMysqli.php' ) ){
     	die( 'No se incluyó libreria conexionMysqli.php' );
@@ -20,7 +20,7 @@ $app->post('/barre_y_envia_clientes_a_administracion_facturacion', function (Req
         die( "No se incluyó : SynchronizationManagmentLog.php" );
     }
     $SynchronizationManagmentLog = new SynchronizationManagmentLog( $link );//instancia clase de Peticiones Log
-echo 'pasa_1';
+//echo 'pasa_1';
   	$link->set_charset("utf8mb4");
     //consulta cabecera de venta
 	$sql = "SELECT 
@@ -174,7 +174,7 @@ echo 'pasa_1';
         array_push( $sales_array, array( "venta"=>$sale_header, "venta_detalle"=>$sale_detail, "cobros"=>$sale_payments, "pagos"=>$sale_payments_detail  ) );
         //$post_data = json_encode( array( "venta"=>$sale_header, "venta_detalle"=>$sale_detail, "cobros"=>$sale_payments, "pagos"=>$sale_payments_detail  ) );
     }
-echo "pasa_2";
+//echo "pasa_2";
     $post_data = json_encode( array( "sales"=>$sales_array ) );
     //return $post_data;
 //consulta el path de API Facturacion 
@@ -182,16 +182,20 @@ echo "pasa_2";
     $stm = $link->query( $sql ) or die( "Error al consultar el path del API de Facturación : {$sql} : {$link->error}" );
     $row = $stm->fetch_assoc();
     $url = "{$row['api_path']}/rest/inserta_venta_facturacion";
+//// echo $url;
     //envia peticion
     $petition = $SynchronizationManagmentLog->sendPetition( $url, $post_data, '' );
     //die( "respuesta : " . $petition );
     $response = json_decode( $petition );
     if( $response->status == 200 ){
-        $sql = "UPDATE ec_pedidos SET id_status_facturacion = 3 WHERE id_pedido = {$sale_header['id_pedido']}";
-        $stm = $link->query( $sql ) or die( "Error al actualizar status de facturacion de la venta : {$sql} : {$link->error}" );
+        foreach ($response->exitosos as $key => $folio) {
+            $sql = "UPDATE ec_pedidos SET id_status_facturacion = 3 WHERE folio_nv = '{$response->exitosos[$key]}'";//echo $sql;
+            $stm = $link->query( $sql ) or die( "Error al actualizar status de facturacion de la venta : {$sql} : {$link->error}" );
+        }
     }else{
         return json_encode( array( "status"=>400, "message"=>"Error al recibir respuesta", "message_detail"=>$petition) );
     }
+    return json_encode( array( "status"=>200, "message"=>"Barrido exitoso." ) );
 });
 
 
